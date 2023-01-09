8d2ab7e80d156d6c0f5bf4c233f6e6f2e5be4778
Zameel Hassan <43750093+zameel7@users.noreply.github.com>
2023-01-09 11:25:55 +0530

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,22 +1,10 @@
 #!/bin/sh
-#------------------------------------------------------------------------------
-# written by: Lawrence McDaniel
-#             https://lawrencemcdaniel.com
-#
-# date:       oct-2022
-#
-# usage:      a work in progress. build package and upload to PyPi.
-#             https://pypi.org/project/edx-oauth2-wordpress-backend/
-#             https://pypi.org/project/edx-oauth2-wordpress-backend-lpm0073/
-#
-# see: https://www.freecodecamp.org/news/how-to-create-and-upload-your-first-python-package-to-pypi/
-#------------------------------------------------------------------------------
 
 python -m pip install --upgrade build
 
 sudo rm -r build
 sudo rm -r dist
-sudo rm -r edx_oauth2_wordpress_backend.egg-info
+sudo rm -r edx_oauth2_auth0_backend.egg-info
 
 python3 -m build --sdist ./
 python3 -m build --wheel ./
@@ -26,6 +14,3 @@ twine check dist/*
 
 # PyPi test
 twine upload --skip-existing --repository testpypi dist/*
-
-# PyPi
-#twine upload --skip-existing dist/*
