2dc11f9971c0b87fbb27a1577339a151fc732807
Zameel Hassan <43750093+zameel7@users.noreply.github.com>
2023-02-05 21:55:58 +0530

    fix: payload["uid"] to payload["sub"]

--- auth0_oauth2/auth0.py
+++ auth0_oauth2/auth0.py
@@ -83,7 +83,7 @@ class BlendEdAuth0OAuth2(BaseOAuth2):
                 "first_name": first_name,
                 "last_name": last_name,
                 "picture": payload["picture"],
-                "user_id": payload["uid"],
+                "user_id": payload["sub"],
             }
         elif "email" not in payload:
             
@@ -97,7 +97,7 @@ class BlendEdAuth0OAuth2(BaseOAuth2):
                 "first_name": first_name,
                 "last_name": last_name,
                 "picture": payload["picture"],
-                "user_id": payload["uid"],
+                "user_id": payload["sub"],
             }
         else:
             # when using an access token, we only have the user_id. We do not have all the other information.
